f69286c53f63ef23951e55e6383e789b43c73bb3
andreicristea <andrei.cristea@apicore.de>
2021-11-20 21:41:38 +0100

    fix: homepage fix for embed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const { board } = window.miro;
 
 async function init() {
   await board.ui.on("icon:click", async () => {
-    await board.ui.openPanel({ pageUrl: "app.html" });
+    await board.ui.openPanel({ pageUrl: "./my-app/build/index.html" });
   });
 }
 
