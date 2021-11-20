3e32cdbefb0a77fcc5e5dbf6344449776d326093
Aly Elalwany <wgl13q@inf.elte.hu>
2021-11-20 12:06:30 +0100

    feat: ParseSQL

--- my-app/src/components/Table.js
+++ my-app/src/components/Table.js
@@ -1,28 +1,56 @@
 import "../App.scss";
 
 function Table() {
-  let parseToSql = (objectToParse) => {
-    console.log();
-    let values = Object.values(objectToParse);
-    let result = "";
+  const parseToSql = (tableName, objectToParse) => {
+    let result = `CREATE TABLE  ${tableName} (`;
+    const columnDataArr = [];
+    for (let columnData of Object.keys(objectToParse)) {
+      let tempRow = '';
+      console.log('column data: ', columnData);
+      tempRow += `${columnData} ${objectToParse[columnData].dataType} `;
+      if (objectToParse[columnData].primary) {
+        tempRow += 'PRIMARY KEY '
+      }
+      columnDataArr.push(tempRow)
+    }
+    result += `${columnDataArr.join(',')})`;
+    return result;
+  }
+  let parseMap = (tablesToParse) => {
+    let listOfTables = Object.keys(tablesToParse);
 
-    for (let i = 0; i < values.length; i++) {
-      console.log(values[i].id);
+    for (let i of listOfTables) {
+      // console.log(i, listOfTables[i][1])
+      console.log(tablesToParse[i])
+      console.log(parseToSql(i, tablesToParse[i]))
+      // console.log(listOfTables[i].map(table => parseToSql(table)))
     }
   };
+  /*CREATE TABLE table_name (
+  column1 datatype,
+  column2 datatype,
+  column3 datatype,
+....
+);
+*/
   let sample = {
     emp: {
       id: { dataType: "int", primary: true, foreign: false },
-      name: { dataType: "varchar(255)", primary: false, foreign: false },
+      name: { dataType: "varchar(255)", primary: true, foreign: false },
       order_id: { dataType: "int", primary: false, foreign: false },
     },
     emp2: {
-      id: { primary: true, foreign: false },
-      name: { primary: false, foreign: false },
-      order_id: { primary: false, foreign: false },
+      id: { dataType: "int", primary: true, foreign: false },
+      name: { dataType: "int", primary: false, foreign: false },
+      order_id: { dataType: "int", primary: false, foreign: false },
+    },
+    emp3: {
+      id: { dataType: "int", primary: true, foreign: false },
+      name: { dataType: "int", primary: false, foreign: false },
+      order_id: { dataType: "int", primary: false, foreign: false },
     },
   };
-  console.log(parseToSql(sample));
+  console.log(parseMap(sample));
   return (
     <div className="Table">
       {/* <div className="table-name">
